6290bfdda09b6aa6ce1c061f72ae2dba932ee59f
Charlie <charlie.eldridge1@gmail.com>
2022-03-21 21:37:52 +0000

    Changes made toHTML, CSS and app.js. fully functioning Etch-a-Sketch app

--- app.js
+++ app.js
@@ -1,45 +1,84 @@
 const container = document.querySelector('.grid');
-const greyBtn = document.querySelector('.btn-grey')
-const rainbowBtn = document.querySelector('.btn-rainbow')
-const blackBtn = document.querySelector('.btn-black')
-const clearBtn = document.querySelector('.btn-clear')
+const greyBtn = document.querySelector('.btn-grey');
+const rainbowBtn = document.querySelector('.btn-rainbow');
+const blackBtn = document.querySelector('.btn-black');
+const clearBtn = document.querySelector('.btn-clear');
+const range = document.querySelector('.range-slider');
+const gridValue = document.querySelector('.grid-value');
 
+
+//Grid function
 function makeGrid(cols, rows) {
-    container.classList.add('new-grid')
-    container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-
-    for(let i = 0; i < rows * cols; i++) {
+    for(let i = 0; i < (rows * cols); i++) {
         let newDiv = document.createElement('div')
-        container.appendChild(newDiv)
-        newDiv.classList.add('grid-item')
-        
-    }
+        container.classList.add('new-grid')
+        container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+        container.appendChild(newDiv).classList.add('grid-item')     
+    };
+};
 
-    
-}
+//Slider to change grid size
+range.addEventListener('click', function() {
 
-makeGrid(16,16);
+    let rangeValue = range.value
+    gridValue.innerText = `${rangeValue} X ${rangeValue}`
+    makeGrid(rangeValue, rangeValue)
 
+    reset();
 
-let gridItem = document.querySelectorAll('.grid-item')
+});
 
+// Buttons
 greyBtn.addEventListener('click', () => {
+    let gridItem = document.querySelectorAll('.grid-item') 
     for(let j = 0; j < gridItem.length; j++) {
         gridItem[j].addEventListener('mouseenter', () => {
-            console.log(gridItem[j])
             gridItem[j].style.background = '#555'
     })
     }
-})
+});
 
+rainbowBtn.addEventListener('click', () => {
+    let gridItem = document.querySelectorAll('.grid-item')  
+    for(let j = 0; j < gridItem.length; j++) {
+        gridItem[j].addEventListener('mouseenter', () => {
+            let rgbOne = Math.floor(Math.random() * 255)
+            let rgbTwo = Math.floor(Math.random() * 255)
+            let rgbThree = Math.floor(Math.random() * 255)
+            gridItem[j].style.background = `rgb(${rgbOne}, ${rgbTwo}, ${rgbThree})`
+    })
+    }
+});
 
+blackBtn.addEventListener('click', () => {
+    let gridItem = document.querySelectorAll('.grid-item')  
+    for(let j = 0; j < gridItem.length; j++) {
+        gridItem[j].addEventListener('mouseenter', () => {
+            gridItem[j].style.background = '#111'
+        })
+    }
+});
 
+clearBtn.addEventListener('click', () => {
+    let gridItem = document.querySelectorAll('.grid-item')  
+    for(let j = 0; j < gridItem.length; j++) {
+        gridItem[j].style.background = '#fff'
+    }
+    reset()
+});
 
+// Reset grid
+function reset() {
+    let gridItem = document.querySelectorAll('.grid-item')
+    gridItem.forEach(element => {
+        element.remove()
+    });
+    makeGrid(range.value, range.value)
+}
 
-
-
-
-
+window.onload = () => {
+    makeGrid(16, 16)
+}
     
    
